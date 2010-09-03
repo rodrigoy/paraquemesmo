6eee9a8b2e967a08d1b73da35f7295fa6fe3e02c
Carlos Villela <cv@lixo.org>
2010-09-03 17:37:51 -0300

    haml, baby!

--- application.rb
+++ application.rb
@@ -1,13 +1,13 @@
 require 'rubygems'
 require 'sinatra'
-require 'erb'
+require 'haml'
 require File.dirname(__FILE__) + '/lib/para_que'
 
 set :views, File.dirname(__FILE__) + '/views'
 
 get '/' do
   @paraque = ParaQue.mesmo?
-  erb :index
+  haml :index
 end
 
 
