4d12d92917f4a1b8626aea85a72728df9b7a1062
Carlos Villela <cv@lixo.org>
2010-09-04 02:02:14 +0800

    rename Paraque -> ParaQue (and paraque -> para_que)

--- lib/para_que.rb
+++ lib/para_que.rb
@@ -1,9 +1,9 @@
-class Paraque
+class ParaQue
 
   @@frases = nil
 
   def self.mesmo?
-    @@frases ||= YAML.load_file(File.dirname(__FILE__) + '/../paraque.yml')
+    @@frases ||= YAML.load_file(File.dirname(__FILE__) + '/../para_que.yml')
     @@frases[Kernel.rand(@@frases.size - 1)]
   end
 
